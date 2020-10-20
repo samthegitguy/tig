8bdf61e5c5183fc103b4c97be31d329f55085e89
samthegitguy <samthegitguy@users.noreply.github.com>
2020-10-20 15:28:36 +1100

    add imports

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,23 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
-shell.exec("echo Hiya!");
+{ // Setup
+    var fs = require('fs');
+    { // Logjs setup
+        var logjs = require('logjs');
+        var logger = new logjs.logger('default');
+        var hi = "runk";
+        { // Transports
+            logger.transports.add(new logjs.transports.standard({
+                colorized: true
+            }));
+            // any stream transport
+            logger.transports.add(new logjs.transports.stream({
+                stream: fs.createWriteStream('./logs/' + Date.now(), {
+                    'flags': 'a'
+                })
+            }));
+        }
+    }
+}
+{ // Main
+    console.log(hi);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,28 @@
-declare var require: any;
+{ // Setup
+    var fs = require('fs');
+    { // Logjs setup
+    var logjs = require('logjs');
+    var logger = new logjs.logger('default');
+    var hi: string = "runk";
+        { // Transports
+            logger.transports.add(new logjs.transports.standard({
+                colorized: true
+            }));
+            
+            // any stream transport
+            logger.transports.add(new logjs.transports.stream({
+                stream: fs.createWriteStream('./logs/' + Date.now(), {
+                    'flags': 'a',
+                })
+            }));
+        }
+    }
+}
+{ // Main
+    switch (process.argv[1]) {
+        
+    }
+}
 
-const shell = require("shelljs");
-shell.exec("echo Hiya!");
+
+
