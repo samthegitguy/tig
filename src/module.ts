1ed9ecd39de927dc30ecbbb8f88973ceb0119b6a
samthegitguy <samthegitguy@users.noreply.github.com>
2020-10-23 14:51:52 +1100

    Return promise

--- src/module.ts
+++ src/module.ts
@@ -1,23 +1,42 @@
-var shell = require("shelljs");
-var init = function () { };
-var snap = function (msg, callback) {
-    var err;
-    var logLocation = ".tig/logs/" + Date.now + ".log";
-    var snapLocation = ".tig/snaps/" + msg;
-    // shell.exec("copy " + snapLocation);
-    console.log('Creating');
-    setTimeout(function () {
-        console.log("Done!");
-    }, 1000);
-    if (err) {
-        callback(true, [0x1]);
-    }
-    else {
-        callback(false, [0x0, snapLocation, logLocation]);
-    }
+
+
+const fs = require("fs");
+
+const logjs = require("logjs");
+const logger = new logjs.logger('default');
+
+const ncp = require("ncp")
+
+
+let init = function () { };
+let snap = function (msg: string) {
+    return new Promise(function(resolve, reject) {
+        // To throw an error, add reject. E.G. to throw a TypeError use reject(new TypeError("Something happened!")); // etc.
+        let now = Date.now(); // Setting date.now in a variable is used for name consistency across program execution
+        let logLocation = ".tig/logs/" + now + ".log";
+        let snapLocation = ".tig/snaps/" + msg;
+        // standard transport
+        logger.transports.add(new logjs.transports.standard({
+            colorized: true
+        }));
+        
+        // any stream transport
+        logger.transports.add(new logjs.transports.stream({
+            stream: fs.createWriteStream(logLocation, {
+                'flags': 'a',
+            })
+        }));
+        // shell.exec("copy " + snapLocation);
+        resolve([0x0, logLocation, snapLocation])
+    }).catch((err) => logger.error("A tig error occured: " + err + ". Please see the owner of this program for more info") )
+
 };
-var down = function () { };
-var up = function () { };
+let down = function () { };
+let up = function () { };
+snap("Hi!")
+    .then(function(result) {
+        console.log("Success:\n" + result)
+    })
 exports.snap = snap;
 exports.down = down;
 exports.up = up;
